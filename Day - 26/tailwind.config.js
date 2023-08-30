d32b36bcfdad1dca614086b8d70f899aed5c4e18
Toufiq Gilani <113186897+Toufiq-Gilani@users.noreply.github.com>
2023-08-30 15:44:25 +0600

    Initial commit

---
+++ Day - 26/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: {
+      'blue': '#0096ff',
+      'purple': '#3f3cbb',
+      'midnight': '#121063',
+      'green': '#3ab7bf',
+      'light': '#78dcca',
+      'gray': '#808080',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
